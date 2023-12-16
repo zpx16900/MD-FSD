35c1f685f2bed6530628a5413929ffc7fe58dea5
zpx16900 <51356515+zpx16900@users.noreply.github.com>
2023-12-16 12:02:35 +0800

    Delete data directory

--- data/datasets/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import builtin  # ensure the builtin datasets are registered
-from .register_coco import register_coco_instances
-
-__all__ = [k for k in globals().keys() if "builtin" not in k and not k.startswith("_")]
